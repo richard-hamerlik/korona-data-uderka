e8c6c8cb9ad2f6e7832eee9a8085c13f7e98de52
Kiko Čabala <kristiancabala@gmail.com>
2020-10-11 16:10:23 +0200

    dumped pairs to csv

--- concat.py
+++ concat.py
@@ -1,25 +1,36 @@
 import json
+import csv
 
 districts_file = 'districts.json'
 villages_file = 'villages.json'
+corona_file = 'pocet_pozitivnych_obce_24_09_2020-07_10_2020.csv'
 
 def get_pairing():
     final_set = []
     with open(districts_file) as dist_stream:
         with open(villages_file) as villages_stream:
-            districts = json.load(dist_stream)
-            villages = json.load(villages_stream)
+            with open(corona_file) as corona_stream:
+                districts = json.load(dist_stream)
+                villages = json.load(villages_stream)
+                corona = csv.reader(corona_stream)
 
-            district_id_to_district = {}
-            for _dist in districts:
-                district_id_to_district[_dist['id']] = _dist
+                district_id_to_district = {}
+                for _dist in districts:
+                    district_id_to_district[_dist['id']] = _dist
 
-            for village in villages:
-                final_set.append({
-                    'village': village,
-                    'district': district_id_to_district[village['district_id']]
-                })
+                for village in villages:
+                    final_set.append({
+                        'village': village,
+                        'district': district_id_to_district[village['district_id']]
+                    })
 
-    return final_set
+                return final_set
 
-print(get_pairing()[:2])
+def dump_set_to_csv(_set, path):
+    with open(path, 'w') as file:
+        writer = csv.writer(file)
+        for obj in _set:
+            writer.writerow([obj['village']['id'], obj['village']['fullname'], obj['district']['id'],obj['district']['name']])
+
+
+dump_set_to_csv(get_pairing(), "pairs.csv")
